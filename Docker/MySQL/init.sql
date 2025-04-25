c3866529f525de3797dede844fd8817576cc4bc7
Yusei0516 <Yusei130516@gmail.com>
2025-04-25 22:04:31 +0900

    管理者登録

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -60,24 +60,25 @@ CREATE TABLE group_messages (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- CREATE TABLE channels (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     uid VARCHAR(255) NOT NULL,
---     name VARCHAR(255) UNIQUE NOT NULL,
---     abstract VARCHAR(255),
---     FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE
--- );
+CREATE TABLE opne_chat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uid VARCHAR(255) NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    abstract VARCHAR(255),
+    FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE
+);
 
--- CREATE TABLE messages (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     uid VARCHAR(255) NOT NULL,
---     cid INT NOT NULL,
---     message TEXT,
---     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE,
---     FOREIGN KEY (cid) REFERENCES channels(id) ON DELETE CASCADE
--- );
+CREATE TABLE opne_chat_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uid VARCHAR(255) NOT NULL,
+    cid INT NOT NULL,
+    message TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE,
+    FOREIGN KEY (cid) REFERENCES opne_chat(id) ON DELETE CASCADE
+);
 
-INSERT INTO users(uid, user_name, email, password) VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
-INSERT INTO channels(id, uid, name, abstract) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
-INSERT INTO messages(id, uid, cid, message) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
+INSERT INTO users (uid, user_name, email, password) VALUES ('admin123', '管理者', 'admin@example.com', '9d73b154738103148a0baae3bb4b0067fbbb230b9cf50c04db70d6393d324c42')
+-- INSERT INTO users(uid, user_name, email, password) VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
+-- INSERT INTO channels(id, uid, name, abstract) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
+-- INSERT INTO messages(id, uid, cid, message) VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
